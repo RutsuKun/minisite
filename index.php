12956a114a4d4df3b0be391941f6d75ac803b00c
FX <ferexio@users.noreply.github.com>
2017-05-18 21:59:28 +0200

    Modified strip_tags for tekst

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <?php
+
 require('config.php');
 require('header.html');
+
 if(empty($_GET['id']))
 {
 while($row = $asd2->fetchArray(SQLITE3_ASSOC)){
-      echo '<hr><center><b><a href="index.php?id='.$nic.'" style="text-decoration: none;">'.$row['tytul'].'</a></b></center><hr>Added at: '.$row['data'].'<br><br>'.strip_tags($row['tekst']).'<br><br>';
+	$tfr = $row['tekst'];
+        $stags = strip_tags($tfr, '<a><img><b><br><hr>');
+	echo '<hr><center><b><a href="index.php?id='.$nic.'" style="text-decoration: none;">'.$row['tytul'].'</a></b></center><hr>Added at: '.$row['data'].'<br><br>'.$stags.'<br><br>';
 	$nic = $nic - 1;
    }
 }
@@ -16,8 +20,10 @@ $gid=$_GET["id"];
 $file = $asd->query('SELECT * FROM asd WHERE ID='.$gid.'');
 if(!empty($file))
 {
-while($row = $file->fetchArray(SQLITE3_ASSOC)){
-echo '<hr><center><b><a href="index.php?id='.$gid.'" style="text-decoration: none;">'.$row['tytul'].'</a></b></center><hr>Added at: '.$row['data'].'<br><br>'.strip_tags($row['tekst']).'<br><br>';
+	while($row = $file->fetchArray(SQLITE3_ASSOC)){
+	$tfr = $row['tekst'];
+	$stags = strip_tags($tfr, '<a><img><b><br><hr>');
+	echo '<hr><center><b><a href="index.php?id='.$gid.'" style="text-decoration: none;">'.$row['tytul'].'</a></b></center><hr>Added at: '.$row['data'].'<br><br>'.$stags.'<br><br>';
 }
 echo'<center><h4><font style="color: blue;"><i>~~~~~~~~~~ Comments ~~~~~~~~~~</i></font></h4></center>';
 }
