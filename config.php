0a428e058e00d262a6b6b3ea70bad4c99c518e05
FX <ferexio@users.noreply.github.com>
2017-05-18 21:07:54 +0200

    Added links to notes

--- config.php
+++ config.php
@@ -1,4 +1,7 @@
 <?php
-$asd = new SQLite3('/path/to/database/file.sqlite');
+$asd = new SQLite3('/sqlite/my.sqlite');
 $asd2 = $asd->query('SELECT * from asd ORDER BY id DESC');
+$ic = $asd->query('SELECT Count(tytul) as count FROM asd');
+$ic2 = $ic->fetchArray(SQLITE3_ASSOC);
+$nic = $ic2['count'];
 ?>
