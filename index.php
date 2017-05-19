65e26b94c8ef798a3753aa51f20219fa9e7cb4fb
FX <ferexio@users.noreply.github.com>
2017-05-19 13:45:44 +0200

    Version 1.2

--- index.php
+++ index.php
@@ -1,14 +1,12 @@
 <?php
-
 require('config.php');
 require('header.html');
-
 if(empty($_GET['id']))
 {
-while($row = $asd2->fetchArray(SQLITE3_ASSOC)){
-	$tfr = $row['tekst'];
-        $stags = strip_tags($tfr, '<a><img><b><br><hr>');
-	echo '<hr><center><b><a href="index.php?id='.$nic.'" style="text-decoration: none;">'.$row['tytul'].'</a></b></center><hr>Added at: '.$row['data'].'<br><br>'.$stags.'<br><br>';
+while($row = $dbs->fetchArray(SQLITE3_ASSOC)){
+	$tfr = $row['news'];
+        $stags = strip_tags($tfr, $tags);
+	echo '<hr><center><b><a href="index.php?id='.$nic.'" style="text-decoration: none;">'.$row['title'].'</a></b></center><hr>Added at: '.$row['date'].'<br><br>'.$stags.'<br><br>';
 	$nic = $nic - 1;
    }
 }
@@ -17,13 +15,13 @@ else
 if (is_numeric($_GET["id"]))
 {
 $gid=$_GET["id"];
-$file = $asd->query('SELECT * FROM asd WHERE ID='.$gid.'');
+$file = $db->query('SELECT * FROM tablename WHERE ID='.$gid.'');
 if(!empty($file))
 {
 	while($row = $file->fetchArray(SQLITE3_ASSOC)){
-	$tfr = $row['tekst'];
-	$stags = strip_tags($tfr, '<a><img><b><br><hr>');
-	echo '<hr><center><b><a href="index.php?id='.$gid.'" style="text-decoration: none;">'.$row['tytul'].'</a></b></center><hr>Added at: '.$row['data'].'<br><br>'.$stags.'<br><br>';
+	$tfr = $row['news'];
+	$stags = strip_tags($tfr, $tags);
+	echo '<hr><center><b><a href="index.php?id='.$gid.'" style="text-decoration: none;">'.$row['title'].'</a></b></center><hr>Added at: '.$row['date'].'<br><br>'.$stags.'<br><br>';
 }
 echo'<center><h4><font style="color: blue;"><i>~~~~~~~~~~ Comments ~~~~~~~~~~</i></font></h4></center>';
 }
@@ -33,6 +31,6 @@ else
 echo 'Notes not found!';
 }
 }
-$asd->close();
+$db->close();
 include('footer.html');
 ?>
