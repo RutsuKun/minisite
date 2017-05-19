3a6fcdef0c6875ad41849c6ad9574db516f1311e
FX <ferexio@users.noreply.github.com>
2017-05-19 13:42:14 +0200

    Version 1.2

--- config.php
+++ config.php
@@ -1,7 +1,8 @@
 <?php
-$asd = new SQLite3('/sqlite/my.sqlite');
-$asd2 = $asd->query('SELECT * from asd ORDER BY id DESC');
-$ic = $asd->query('SELECT Count(tytul) as count FROM asd');
+$db = new SQLite3('/path/to/file.sqlite');
+$dbs = $db->query('SELECT * from tablename ORDER BY id DESC');
+$ic = $db->query('SELECT Count(title) as count FROM tablename');
 $ic2 = $ic->fetchArray(SQLITE3_ASSOC);
 $nic = $ic2['count'];
+$tags='<a><img><b><br><hr><s><i>';
 ?>
