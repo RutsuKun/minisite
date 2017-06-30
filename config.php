3372db02ae3e7b62895de3cdcaa3aa4b4cee32bf
FX <ferexio@users.noreply.github.com>
2017-06-30 17:32:44 +0200

    Mini update

--- config.php
+++ config.php
@@ -4,7 +4,6 @@ $page = $_GET['page'];
 $dbs = $db->query("SELECT * from tablename ORDER BY UPPER(id) DESC LIMIT 2 OFFSET 2*'$page'-2");
 $ic = $db->query('SELECT Count(title) as count FROM tablename');
 $ic2 = $ic->fetchArray(SQLITE3_ASSOC);
-$nic = $ic2['count'];
 $tags='<a><img><b><br><hr><s><i>';
 $app = 2;
 ?>
